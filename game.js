d957cd59014b687202b150f0c331847889d220c6
Luke <luke@luke.co.pl>
2017-12-04 22:46:45 +0100

    Add controls information, pause mode, minor CSS fix

--- game.js
+++ game.js
@@ -8,7 +8,9 @@ let container = document.getElementById('game-container');
 container.insertBefore(canvas, container.firstChild);
 
 function newGame() {
-    let gameOver = false;
+    let gameOver = false,
+        gamePaused = false,
+        showControlsHint = true;
 
     const groundPosY = canvas.height - groundHeight;
 
@@ -33,8 +35,16 @@ function newGame() {
         }
         pressedKeys[e.keyCode] = true;
 
-        if (e.keyCode == 32) {
-            horse.startJump();
+        if (!gameOver) {
+            switch (e.keyCode) {
+                case 32: // spacebar
+                    horse.startJump();
+                    showControlsHint = false;
+                    break;
+                case 80: // P
+                    gamePaused = !gamePaused;
+                    break;
+            }
         }
     }
     document.addEventListener('keydown', keyDownListener);
@@ -48,6 +58,10 @@ function newGame() {
     }
     document.addEventListener('keyup', keyUpListener);
 
+    window.onblur = function() {
+        gamePaused = true;
+    }
+
     function checkCollision(first, second) {
         let rect1 = first.collisionRect(),
             rect2 = second.collisionRect();
@@ -60,33 +74,35 @@ function newGame() {
     }
 
     (function update() {
-        horse.update();
-        skeletons = skeletons.filter(s => s.pos_x > -100);
-        if (!gameOver) {
-            if (Date.now() - last_skeleton_added > 1200) {
-                if (Math.random() > 0.6) {
-                    skeletons.push(new Skeleton(canvas.width, groundPosY));
+        if (!gamePaused) {
+            horse.update();
+            skeletons = skeletons.filter(s => s.pos_x > -100);
+            if (!gameOver) {
+                if (Date.now() - last_skeleton_added > 1200) {
+                    if (Math.random() > 0.6) {
+                        skeletons.push(new Skeleton(canvas.width, groundPosY));
+                    }
+                    last_skeleton_added = Date.now();
                 }
-                last_skeleton_added = Date.now();
             }
-        }
-        skeletons.forEach(s => {
-            s.update();
-            if (checkCollision(horse, s)) {
-                gameOver = true;
-                s.attack();
-                horse.stop();
+            skeletons.forEach(s => {
+                s.update();
+                if (checkCollision(horse, s)) {
+                    gameOver = true;
+                    s.attack();
+                    horse.stop();
+                }
+            });
+            if (gameOver) {
+                skeletons.forEach(s => s.stop());
+                setTimeout(restartGame, 3000);
             }
-        });
-        if (gameOver) {
-            skeletons.forEach(s => s.stop());
-            setTimeout(restartGame, 3000);
         }
         setTimeout(update, 50);
     })();
 
     (function updateBackground() {
-        if (!gameOver) {
+        if (!gameOver && !gamePaused) {
             updateGround();
             updateTrees(difficulty);
             if (difficulty < 1) {
@@ -111,6 +127,37 @@ function newGame() {
         ctx.fillStyle = 'white';
         ctx.font = '16px monospace';
         ctx.fillText(`Difficulty: ${Math.round(difficulty * 100)}%`, 10, 20);
+
+        if (showControlsHint && !gameOver && !gamePaused) {
+            ctx.save();
+            ctx.fillStyle = 'white';
+            ctx.textAlign = 'center';
+            ctx.textBaseline = 'middle';
+            ctx.font = '28px "Open Sans"';
+            let text = "Hold spacebar to jump, release to land.",
+                text2 = "Press P to toggle pause.";
+            ctx.fillText(text, canvas.width / 2, 100);
+            ctx.fillText(text2, canvas.width / 2, 140);
+            ctx.restore();
+        }
+
+        if (gamePaused) {
+            ctx.save();
+            ctx.fillStyle = 'rgba(237, 116, 13, 0.1)';
+            ctx.fillRect(0, 0, canvas.width, canvas.height);
+            ctx.fillStyle = 'rgb(237, 116, 13)';
+            ctx.textAlign = 'center';
+            ctx.textBaseline = 'middle';
+            ctx.font = '64px "Open Sans"';
+            ctx.fillText('PAUSED', canvas.width / 2, canvas.height / 2);
+            ctx.strokeStyle = 'white';
+            ctx.lineWidth = 2;
+            ctx.strokeText('PAUSED', canvas.width / 2, canvas.height / 2);
+            ctx.fillStyle = 'white';
+            ctx.font = '24px "Open Sans"';
+            ctx.fillText("Press P to continue.", canvas.width / 2, canvas.height - 80);
+            ctx.restore();
+        }
 
         if (gameOver) {
             ctx.save();
